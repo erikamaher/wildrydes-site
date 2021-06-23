9dad012fbadebcc2c2d8b115ac267c77babffceb
erikamaher <85901590+erikamaher@users.noreply.github.com>
2021-06-23 13:43:45 -0400

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_nTcURWdNY', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '3hia397kiqc8sse39ljpu4b5pd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
